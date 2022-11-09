c1a72311291e95b90248ae67c5f6f48f4210cdde
maj-jinnah <maj.jinnah@gmail.com>
2022-11-09 17:19:26 +0600

    env file added and run function added

--- index.js
+++ index.js
@@ -10,21 +10,24 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ea93pzh.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+
+    try{
+
+    }
+    finally{
+        
+    }
+}
+run().catch(error => console.error(error));
 
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.send("Now server is running!");
 });
 
-
-
-app.listen(Port, () =>{
+app.listen(Port, () => {
     console.log(`server is running, ${Port}`);
 });
