bc74ee3bb800a2747194a99669eaca7e1266a342
maj-jinnah <maj.jinnah@gmail.com>
2022-11-09 22:18:30 +0600

    services added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const Port = process.env.Port || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -14,11 +14,24 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
 
-    try{
+    try {
+        const serviceCollection = client.db('picturesque').collection('services');
 
+        app.get('/services', async (req, res) =>{
+            const query = {}
+            const cursor  = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+        app.get('/services/:id', async(req,res) =>{
+            const id = req.params.id
+            const query = {_id : ObjectId(id)}
+            const service  = await serviceCollection.findOne(query)
+            res.send(service)
+        })
     }
-    finally{
-        
+    finally {
+
     }
 }
 run().catch(error => console.error(error));
